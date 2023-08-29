0fd4c272c5498474578c8e7adb635b84ca1c4ab3
Aleksandr <99916190+AleksandrFisun@users.noreply.github.com>
2023-08-29 10:58:01 +0300

    update

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,19 +1,57 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from 'components/Header/Header.jsx';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Sidebar from 'components/Sidebar/Sidebar';
 import styles from 'styles/index.module.scss';
 
 export const SharedLayout = () => {
   const [menuActive, setMenuActive] = useState(false);
 
+  useEffect(() => {
+    const hendleEscClose = event => {
+      if (event.code === 'Escape' && window.innerWidth <= 1439) {
+        setMenuActive(false);
+      }
+    };
+
+    window.addEventListener('keydown', hendleEscClose);
+
+    return () => {
+      window.removeEventListener('keydown', hendleEscClose);
+    };
+  }, [menuActive, setMenuActive]);
+
+  useEffect(() => {
+    const handleMinXlSize = () => {
+      if (window.innerWidth >= 1440) {
+        setMenuActive(true);
+      } else {
+        setMenuActive(false);
+      }
+    };
+
+    handleMinXlSize();
+
+    window.addEventListener('resize', handleMinXlSize);
+
+    return () => {
+      window.removeEventListener('resize', handleMinXlSize);
+    };
+  }, [setMenuActive]);
+
   const handleClick = () => {
     setMenuActive(!menuActive);
   };
 
+  const hendleBackdropClose = event => {
+    if (event.target === event.currentTarget && window.innerWidth <= 1439) {
+      setMenuActive(false);
+    }
+  };
+
   return (
-    <div className={styles.AFNavResWr}>
+    <div className={styles.AFNavResWr} onClick={hendleBackdropClose}>
       <header className={styles.AFNavResWrHead}>
         <Header click={handleClick} />
       </header>
