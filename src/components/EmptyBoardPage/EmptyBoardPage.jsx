4a3748fa23d5dc8931938723bfe77a75702cefcb
ArtemOsypov <artem.osypov@gmail.com>
2023-09-05 01:53:01 +0300

    fix modal, add style to emptyPage

--- src/components/EmptyBoardPage/EmptyBoardPage.jsx
+++ src/components/EmptyBoardPage/EmptyBoardPage.jsx
@@ -1,8 +1,10 @@
 import scss from 'styles/index.module.scss';
+import { useAuth } from 'hooks';
 
 export const EmptyBoardPage = () => {
+  const { user } = useAuth();
   return (
-    <div className={scss.AOemptyPage}>
+    <div className={scss.AOemptyPage} data-theme={user.theme}>
         <p className={scss.AOemptyPageText}>
            Before starting your project, it is essential <span className={scss.AOcreateBoard}>to create a board</span> to
            visualize and track all the necessary tasks and milestones. This board
