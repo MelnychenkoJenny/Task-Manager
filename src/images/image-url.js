d9953cbab6b73d02cd436b5c27d5065124dff745
K-Basket <koshykkv@gmail.com>
2023-09-04 16:54:52 +0300

    added image-url

---
+++ src/images/image-url.js
@@ -0,0 +1,141 @@
+const BASE_URL = 'https://res.cloudinary.com/daqnavarl/image/upload/';
+
+// Desctop
+export const desctopAbstraction2x = `${BASE_URL}v1693828274/Task%20Pro%20-%20Project/Desctop/abstraction-2x_btdjny.jpg`;
+export const desctopAbstraction = `${BASE_URL}v1693828793/Task%20Pro%20-%20Project/Desctop/abstraction_hxzxl6.jpg`;
+export const desctopBaloonsMore2x = `${BASE_URL}v1693829299/Task%20Pro%20-%20Project/Desctop/baloons-more-2x_iu1rmy.jpg`;
+export const desctopBaloonsMore = `${BASE_URL}v1693829446/Task%20Pro%20-%20Project/Desctop/baloons-more_gb62jn.jpg`;
+export const desctopBaloons = `${BASE_URL}v1693829516/Task%20Pro%20-%20Project/Desctop/baloons_m6sacm.jpg`;
+export const desctopBaloons2x = `${BASE_URL}v1693829580/Task%20Pro%20-%20Project/Desctop/baloons2x_ralsal.jpg`;
+export const desctopBorrowPit2x = `${BASE_URL}v1693829650/Task%20Pro%20-%20Project/Desctop/borrow-pit-2x_xjypao.jpg`;
+export const desctopBorrowPit = `${BASE_URL}v1693829710/Task%20Pro%20-%20Project/Desctop/borrow-pit_ca8k9c.jpg`;
+export const desctopCamping2x = `${BASE_URL}v1693829773/Task%20Pro%20-%20Project/Desctop/camping-2x_tbu4o3.jpg`;
+export const desctopCamping = `${BASE_URL}v1693829855/Task%20Pro%20-%20Project/Desctop/camping_nqi3zq.jpg`;
+export const desctopFlowers2x = `${BASE_URL}v1693830037/Task%20Pro%20-%20Project/Desctop/flowers-2x_hfifem.jpg`;
+export const desctopFlowers = `${BASE_URL}v1693830129/Task%20Pro%20-%20Project/Desctop/flowers_csjlkm.jpg`;
+export const desctopGorge2x = `${BASE_URL}v1693830192/Task%20Pro%20-%20Project/Desctop/gorge-2x_qzzmcy.jpg`;
+export const desctopGorge = `${BASE_URL}v1693830230/Task%20Pro%20-%20Project/Desctop/gorge_vtdell.jpg`;
+export const desctopLeaves2x = `${BASE_URL}v1693830272/Task%20Pro%20-%20Project/Desctop/leaves-2x_m4oq0q.jpg`;
+export const desctopLeaves = `${BASE_URL}v1693830318/Task%20Pro%20-%20Project/Desctop/leaves_qbbd37.jpg`;
+export const desctopMoon2x = `${BASE_URL}v1693830368/Task%20Pro%20-%20Project/Desctop/moon-2x_hmrdkp.jpg`;
+export const desctopMoon = `${BASE_URL}v1693830414/Task%20Pro%20-%20Project/Desctop/moon_waa8he.jpg`;
+export const desctopMountains2x = `${BASE_URL}v1693830455/Task%20Pro%20-%20Project/Desctop/mountains-2x_cwwmbq.jpg`;
+export const desctopMountains = `${BASE_URL}v1693830507/Task%20Pro%20-%20Project/Desctop/mountains_m5l4hd.jpg`;
+export const desctopSakura2x = `${BASE_URL}v1693830549/Task%20Pro%20-%20Project/Desctop/sakura-2x_aaslkb.jpg`;
+export const desctopSakura = `${BASE_URL}v1693830596/Task%20Pro%20-%20Project/Desctop/sakura_ka3wur.jpg`;
+export const desctopSea2x = `${BASE_URL}v1693830639/Task%20Pro%20-%20Project/Desctop/sea-2x_iv1kn4.jpg`;
+export const desctopSea = `${BASE_URL}v1693830683/Task%20Pro%20-%20Project/Desctop/sea_izgon1.jpg`;
+export const desctopShip2x = `${BASE_URL}v1693830715/Task%20Pro%20-%20Project/Desctop/ship-2x_enddtt.jpg`;
+export const desctopShip = `${BASE_URL}v1693830817/Task%20Pro%20-%20Project/Desctop/ship_vvafs5.jpg`;
+export const desctopSky2x = `${BASE_URL}v1693830851/Task%20Pro%20-%20Project/Desctop/sky-2x_qkzjyu.jpg`;
+export const desctopSky = `${BASE_URL}v1693830899/Task%20Pro%20-%20Project/Desctop/sky_rl4rbm.jpg`;
+export const desctopYoungMoon2x = `${BASE_URL}v1693830932/Task%20Pro%20-%20Project/Desctop/young-moon-2x_pnz23e.jpg`;
+export const desctopYoungMoon = `${BASE_URL}v1693831000/Task%20Pro%20-%20Project/Desctop/young-moon_ca508x.jpg`;
+
+// Tablet
+export const tabletAbstraction2x = `${BASE_URL}v1693831240/Task%20Pro%20-%20Project/Tablet/abstraction-2x_guuzym.jpg`;
+export const tabletAbstraction = `${BASE_URL}v1693831292/Task%20Pro%20-%20Project/Tablet/abstraction_attswv.jpg`;
+export const tabletBaloonsMore2x = `${BASE_URL}v1693831328/Task%20Pro%20-%20Project/Tablet/baloons-more-2x_spcyx0.jpg`;
+export const tabletBaloonsMore = `${BASE_URL}v1693831357/Task%20Pro%20-%20Project/Tablet/baloons-more_wpvzbd.jpg`;
+export const tabletBaloons = `${BASE_URL}v1693831386/Task%20Pro%20-%20Project/Tablet/baloons_pxkwrd.jpg`;
+export const tabletBaloons2x = `${BASE_URL}v1693831431/Task%20Pro%20-%20Project/Tablet/baloons2x_ejkkyf.jpg`;
+export const tabletBorrowPit2x = `${BASE_URL}v1693831470/Task%20Pro%20-%20Project/Tablet/borrow-pit-2x_mjpku8.jpg`;
+export const tabletBorrowPit = `${BASE_URL}v1693831501/Task%20Pro%20-%20Project/Tablet/borrow-pit_tvurda.jpg`;
+export const tabletCamping2x = `${BASE_URL}v1693831533/Task%20Pro%20-%20Project/Tablet/camping-2x_urmhnj.jpg`;
+export const tabletCamping = `${BASE_URL}v1693831564/Task%20Pro%20-%20Project/Tablet/camping_xdjbwd.jpg`;
+export const tabletFlowers2x = `${BASE_URL}v1693831604/Task%20Pro%20-%20Project/Tablet/flowers-2x_xvhwda.jpg`;
+export const tabletFlowers = `${BASE_URL}v1693831634/Task%20Pro%20-%20Project/Tablet/flowers_boghc8.jpg`;
+export const tabletGorge2x = `${BASE_URL}v1693831663/Task%20Pro%20-%20Project/Tablet/gorge-2x_yo1y6v.jpg`;
+export const tabletGorge = `${BASE_URL}v1693831691/Task%20Pro%20-%20Project/Tablet/gorge_zck8ne.jpg`;
+export const tabletLeaves2x = `${BASE_URL}v1693831768/Task%20Pro%20-%20Project/Tablet/leaves-2x_q5uimi.jpg`;
+export const tabletLeaves = `${BASE_URL}v1693831804/Task%20Pro%20-%20Project/Tablet/leaves_dwmr4v.jpg`;
+export const tabletMoon2x = `${BASE_URL}v1693831842/Task%20Pro%20-%20Project/Tablet/moon-2x_mujnch.jpg`;
+export const tabletMoon = `${BASE_URL}v1693831878/Task%20Pro%20-%20Project/Tablet/moon_wqxcjl.jpg`;
+export const tabletMountains2x = `${BASE_URL}v1693831906/Task%20Pro%20-%20Project/Tablet/mountains-2x_d6fa1t.jpg`;
+export const tabletMountains = `${BASE_URL}v1693831945/Task%20Pro%20-%20Project/Tablet/mountains_r6wbuq.jpg`;
+export const tabletSakura2x = `${BASE_URL}v1693831987/Task%20Pro%20-%20Project/Tablet/sakura-2x_ybxxms.jpg`;
+export const tabletSakura = `${BASE_URL}v1693832028/Task%20Pro%20-%20Project/Tablet/sakura_sm9era.jpg`;
+export const tabletSea2x = `${BASE_URL}v1693832065/Task%20Pro%20-%20Project/Tablet/sea-2x_yuaszh.jpg`;
+export const tabletSea = `${BASE_URL}v1693832094/Task%20Pro%20-%20Project/Tablet/sea_iftqgz.jpg`;
+export const tabletShip2x = `${BASE_URL}v1693832129/Task%20Pro%20-%20Project/Tablet/ship-2x_j4chrh.jpg`;
+export const tabletShip = `${BASE_URL}v1693832158/Task%20Pro%20-%20Project/Tablet/ship_ibh9y4.jpg`;
+export const tabletSky2x = `${BASE_URL}v1693832198/Task%20Pro%20-%20Project/Tablet/sky-2x_x618ey.jpg`;
+export const tabletSky = `${BASE_URL}v1693832239/Task%20Pro%20-%20Project/Tablet/sky_nvmm1k.jpg`;
+export const tabletYoungMoon2x = `${BASE_URL}v1693832267/Task%20Pro%20-%20Project/Tablet/young-moon-2x_trawtp.jpg`;
+export const tabletYoungMoon = `${BASE_URL}v1693832303/Task%20Pro%20-%20Project/Tablet/young-moon_nf6xuz.jpg`;
+
+// Mobile
+export const mobileAbstraction2x = `${BASE_URL}v1693832485/Task%20Pro%20-%20Project/Mobile/abstraction-2x_yfafan.jpg`;
+export const mobileAbstraction = `${BASE_URL}v1693832530/Task%20Pro%20-%20Project/Mobile/abstraction_mcmcs0.jpg`;
+export const mobileBaloonsMore2x = `${BASE_URL}v1693832568/Task%20Pro%20-%20Project/Mobile/baloons-more-2x_znjjvt.jpg`;
+export const mobileBaloonsMore = `${BASE_URL}v1693832600/Task%20Pro%20-%20Project/Mobile/baloons-more_nfobhj.jpg`;
+export const mobileBaloons = `${BASE_URL}v1693832868/Task%20Pro%20-%20Project/Mobile/baloons_u2syv1.jpg`;
+export const mobileBaloons2x = `${BASE_URL}v1693832898/Task%20Pro%20-%20Project/Mobile/baloons2x_zezong.jpg`;
+export const mobileBorrowPit2x = `${BASE_URL}v1693832930/Task%20Pro%20-%20Project/Mobile/borrow-pit-2x_eb25yp.jpg`;
+export const mobileBorrowPit = `${BASE_URL}v1693832961/Task%20Pro%20-%20Project/Mobile/borrow-pit_uhxrfc.jpg`;
+export const mobileCamping2x = `${BASE_URL}v1693832990/Task%20Pro%20-%20Project/Mobile/camping-2x_g1zbpy.jpg`;
+export const mobileCamping = `${BASE_URL}v1693833024/Task%20Pro%20-%20Project/Mobile/camping_eoahjg.jpg`;
+export const mobileFlowers2x = `${BASE_URL}v1693833057/Task%20Pro%20-%20Project/Mobile/flowers-2x_fjrahx.jpg`;
+export const mobileFlowers = `${BASE_URL}v1693833088/Task%20Pro%20-%20Project/Mobile/flowers_o8qyup.jpg`;
+export const mobileGorge2x = `${BASE_URL}v1693833128/Task%20Pro%20-%20Project/Mobile/gorge-2x_dlakxb.jpg`;
+export const mobileGorge = `${BASE_URL}v1693833160/Task%20Pro%20-%20Project/Mobile/gorge_jkwktq.jpg`;
+export const mobileLeaves2x = `${BASE_URL}v1693833194/Task%20Pro%20-%20Project/Mobile/leaves-2x_xlmt5a.jpg`;
+export const mobileLeaves = `${BASE_URL}v1693833224/Task%20Pro%20-%20Project/Mobile/leaves_r1bogu.jpg`;
+export const mobileMoon2x = `${BASE_URL}v1693833256/Task%20Pro%20-%20Project/Mobile/moon-2x_hsgvst.jpg`;
+export const mobileMoon = `${BASE_URL}v1693833290/Task%20Pro%20-%20Project/Mobile/moon_mdyvf2.jpg`;
+export const mobileMountains2x = `${BASE_URL}v1693833321/Task%20Pro%20-%20Project/Mobile/mountains-2x_yduyiz.jpg`;
+export const mobileMountains = `${BASE_URL}v1693833353/Task%20Pro%20-%20Project/Mobile/mountains_aipalu.jpg`;
+export const mobileSakura2x = `${BASE_URL}v1693833385/Task%20Pro%20-%20Project/Mobile/sakura-2x_xziaaa.jpg`;
+export const mobileSakura = `${BASE_URL}v1693833420/Task%20Pro%20-%20Project/Mobile/sakura_i2l65i.jpg`;
+export const mobileSea2x = `${BASE_URL}v1693833451/Task%20Pro%20-%20Project/Mobile/sea-2x_o8yuf8.jpg`;
+export const mobileSea = `${BASE_URL}v1693833481/Task%20Pro%20-%20Project/Mobile/sea_sntvsr.jpg`;
+export const mobileShip2x = `${BASE_URL}v1693833522/Task%20Pro%20-%20Project/Mobile/ship-2x_tydmwn.jpg`;
+export const mobileShip = `${BASE_URL}v1693833513/Task%20Pro%20-%20Project/Mobile/ship_vj3how.jpg`;
+export const mobileSky2x = `${BASE_URL}v1693833580/Task%20Pro%20-%20Project/Mobile/sky-2x_jvgk7w.jpg`;
+export const mobileSky = `${BASE_URL}v1693833588/Task%20Pro%20-%20Project/Mobile/sky_xotjjt.jpg`;
+export const mobileYoungMoon2x = `${BASE_URL}v1693833635/Task%20Pro%20-%20Project/Mobile/young-moon-2x_rqkvrv.jpg`;
+export const mobileYoungMoon = `${BASE_URL}v1693833627/Task%20Pro%20-%20Project/Mobile/young-moon_hjm3we.jpg`;
+
+// Welcome Page
+export const welcomePageWelcomeDesctop2x = `${BASE_URL}v1693833734/Task%20Pro%20-%20Project/Welcome%20Page/welcome-desctop-2x_x5js3l.png`;
+export const welcomePageWelcomeDesctop = `${BASE_URL}v1693833742/Task%20Pro%20-%20Project/Welcome%20Page/welcome-desctop_dt0p8r.png`;
+export const welcomePageWelcomeMobile2x = `${BASE_URL}v1693833758/Task%20Pro%20-%20Project/Welcome%20Page/welcome-mobile-2x_xgsbtr.png`;
+export const welcomePageWelcomeMobile = `${BASE_URL}v1693833750/Task%20Pro%20-%20Project/Welcome%20Page/welcome-mobile_gb2a2f.png`;
+export const welcomePageWelcomeTablet2x = `${BASE_URL}v1693833767/Task%20Pro%20-%20Project/Welcome%20Page/welcome-tablet-2x_d46gsn.png`;
+export const welcomePageWelcomeTablet = `${BASE_URL}v1693833775/Task%20Pro%20-%20Project/Welcome%20Page/welcome-tablet_nsxssw.png`;
+
+// Icon Modal Bg
+export const iconModalBgAbstraction = `${BASE_URL}v1693834062/Task%20Pro%20-%20Project/Icon%20Modal%20Bg/abstraction_tpryse.png`;
+export const iconModalBgBaloonsMore = `${BASE_URL}v1693834070/Task%20Pro%20-%20Project/Icon%20Modal%20Bg/baloons-more_mcouai.png`;
+export const iconModalBgBaloons = `${BASE_URL}v1693834079/Task%20Pro%20-%20Project/Icon%20Modal%20Bg/baloons_fqr2fh.png`;
+export const iconModalBgBorrowPit = `${BASE_URL}v1693834087/Task%20Pro%20-%20Project/Icon%20Modal%20Bg/borrow-pit_jt37iw.png`;
+export const iconModalBgCamping = `${BASE_URL}v1693834096/Task%20Pro%20-%20Project/Icon%20Modal%20Bg/camping_yq1chl.png`;
+export const iconModalBgDefaultDark = `${BASE_URL}v1693834104/Task%20Pro%20-%20Project/Icon%20Modal%20Bg/default-dark_zsqglz.png`;
+export const iconModalBgDefaultLight = `${BASE_URL}v1693834121/Task%20Pro%20-%20Project/Icon%20Modal%20Bg/default-light_x8hzsj.png`;
+export const iconModalBgDefaultViolet = `${BASE_URL}v1693834113/Task%20Pro%20-%20Project/Icon%20Modal%20Bg/default-violet_mrje2r.png`;
+export const iconModalBgFlowers = `${BASE_URL}v1693834130/Task%20Pro%20-%20Project/Icon%20Modal%20Bg/flowers_cv176l.png`;
+export const iconModalBgGorge = `${BASE_URL}v1693834138/Task%20Pro%20-%20Project/Icon%20Modal%20Bg/gorge_ugzvei.png`;
+export const iconModalBgLeaves = `${BASE_URL}v1693834146/Task%20Pro%20-%20Project/Icon%20Modal%20Bg/leaves_dcl1xa.png`;
+export const iconModalBgMoon = `${BASE_URL}v1693834163/Task%20Pro%20-%20Project/Icon%20Modal%20Bg/moon_ujzi7j.png`;
+export const iconModalBgMountains = `${BASE_URL}v1693834155/Task%20Pro%20-%20Project/Icon%20Modal%20Bg/mountains_cxz5w3.png`;
+export const iconModalBgSacura = `${BASE_URL}v1693834172/Task%20Pro%20-%20Project/Icon%20Modal%20Bg/sacura_quddsn.png`;
+export const iconModalBgSea = `${BASE_URL}v1693834181/Task%20Pro%20-%20Project/Icon%20Modal%20Bg/sea_bpxv9b.png`;
+export const iconModalBgShip = `${BASE_URL}v1693834189/Task%20Pro%20-%20Project/Icon%20Modal%20Bg/ship_huc7tr.png`;
+export const iconModalBgSky = `${BASE_URL}v1693834206/Task%20Pro%20-%20Project/Icon%20Modal%20Bg/sky_cftt9c.png`;
+export const iconModalBgYoungMoon = `${BASE_URL}v1693834197/Task%20Pro%20-%20Project/Icon%20Modal%20Bg/young-moon_gaeefv.png`;
+
+// Person
+export const personPerson1x = `${BASE_URL}v1693834708/Task%20Pro%20-%20Project/Person/person_1x_axwt4m.png`;
+export const personPerson2x = `${BASE_URL}v1693834716/Task%20Pro%20-%20Project/Person/person_2x_ydwkgr.png`;
+export const personPerson3x = `${BASE_URL}v1693834724/Task%20Pro%20-%20Project/Person/person_3x_tje1i3.png`;
+
+// Other
+export const userDark = `${BASE_URL}v1693834914/Task%20Pro%20-%20Project/Other/user-dark_ai5llv.jpg`;
+export const userLight = `${BASE_URL}v1693834923/Task%20Pro%20-%20Project/Other/user-light_xlukku.jpg`;
+export const userViolet = `${BASE_URL}v1693834931/Task%20Pro%20-%20Project/Other/user-violet_msusyg.jpg`;
+export const helpCactus2x = `${BASE_URL}v1693834949/Task%20Pro%20-%20Project/Other/help-cactus-2x_q9tz1l.png`;
+export const helpCactus = `${BASE_URL}v1693834958/Task%20Pro%20-%20Project/Other/help-cactus_bfbyck.png`;
+export const icon = `${BASE_URL}v1693834975/Task%20Pro%20-%20Project/Other/icon_szpmhm.png`;
+export const userDefaultDark = `${BASE_URL}v1693834966/Task%20Pro%20-%20Project/Other/user-default-dark_ogkv91.png`;
+export const userDefaultLight = `${BASE_URL}v1693834984/Task%20Pro%20-%20Project/Other/user-default-light_yewj3z.png`;
+export const userDefaultViolet = `${BASE_URL}v1693834993/Task%20Pro%20-%20Project/Other/user-default-violet_hajvot.png`;
