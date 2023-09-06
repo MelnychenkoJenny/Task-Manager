e7468dccd8f3a03e93d6a29faff521f3476ba674
K-Basket <koshykkv@gmail.com>
2023-09-06 12:41:05 +0300

    fix Filter

--- src/components/MainDashboard/Filters.jsx
+++ src/components/MainDashboard/Filters.jsx
@@ -11,41 +11,47 @@ export const Filters = ({ className }) => {
   const { boardById } = useBoards();
   const { user } = useAuth();
 
-  
   const handleShowAll = () => {
-    console.log('Filtered tasks are shown')
-  }
+    console.log('Filtered tasks are shown');
+  };
 
   const onOpenFilter = () => {
     setIsFilterVisible(true);
   };
   const onCloseFilter = () => {
     setIsFilterVisible(false);
-  }; 
+  };
 
   useEffect(() => {
     const handleKeyDown = e => {
       if (e.code === 'Escape') {
         setIsFilterVisible(false);
-     };
+      }
     };
     window.addEventListener('keydown', handleKeyDown);
     return () => window.removeEventListener('keydown', handleKeyDown);
   }, [isFilterVisible]);
 
-
   return (
-    <div className={`${className} ${styles.OBFilterModalContainer}`} data-theme={user.theme}>
+    <div
+      className={`${className} ${styles.OBFilterModalContainer}`}
+      data-theme={user.theme}
+    >
       <div className={styles.KkWrapFilters}>
         <h2
           className={styles.KkTitle}
-          style={{ padding: !boardById.background && '4px' }}
+          style={{ padding: boardById.background && '4px' }}
         >
           {boardById.title}
         </h2>
         {/* <h2 className={styles.KkTitle}>{boardById.title}</h2> */}
 
-        <button type='button' className={styles.KkBtnFilters} onClick={onOpenFilter}>
+        <button
+          type="button"
+          className={styles.KkBtnFilters}
+          style={{ padding: boardById.background && '4px' }}
+          onClick={onOpenFilter}
+        >
           <svg width="16px" height="16px">
             <use href={`${SvgSprite}#icon-filter`}></use>
           </svg>
@@ -53,20 +59,31 @@ export const Filters = ({ className }) => {
         </button>
 
         {isFilterVisible && (
-            <div className={styles.OBFiltersModalContainer} style={{ opacity: isFilterVisible ? 1 : 0 }} data-theme={user.theme}>
-              <button type='button' className={styles.OBFilterBtnIcon} aria-label='close' onClick={onCloseFilter}>
-                <svg  width="18" height="18">
-                    <use className='icon' href={SvgSprite + '#icon-close'} />
-                </svg>
-              </button>
-              <h4 className={styles.OBFiltersModalTitle}>Filters</h4>
-              <hr className={styles.OBFiltersModalSeparator} />  
-              <div className={styles.OBFiltersModalContent}>
-                  <RadioBtns parentComponent={'FiltersModal'} />
-                  <p className={styles.OBFiltersModalShow} onClick={handleShowAll}>Show all</p>
-              </div>
+          <div
+            className={styles.OBFiltersModalContainer}
+            style={{ opacity: isFilterVisible ? 1 : 0 }}
+            data-theme={user.theme}
+          >
+            <button
+              type="button"
+              className={styles.OBFilterBtnIcon}
+              aria-label="close"
+              onClick={onCloseFilter}
+            >
+              <svg width="18" height="18">
+                <use className="icon" href={SvgSprite + '#icon-close'} />
+              </svg>
+            </button>
+            <h4 className={styles.OBFiltersModalTitle}>Filters</h4>
+            <hr className={styles.OBFiltersModalSeparator} />
+            <div className={styles.OBFiltersModalContent}>
+              <RadioBtns parentComponent={'FiltersModal'} />
+              <p className={styles.OBFiltersModalShow} onClick={handleShowAll}>
+                Show all
+              </p>
             </div>
-        )} 
+          </div>
+        )}
       </div>
     </div>
   );
