76d9a9cec255199bf3d6418fafd83d00c3c7b281
MelnychenkoJenny <melnichenkojenny@gmail.com>
2023-10-01 12:02:38 +0300

    fix styles loader

--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -1,9 +1,12 @@
 import RegistrationForm from './RegistrationForm';
 import styles from 'styles/index.module.scss';
+import { useAuth } from 'hooks';
 
 const Registration = () => {
+  const { user } = useAuth();
+  
   return (
-    <section className={styles.AfWelcomReg}>
+    <section className={styles.AfWelcomReg} data-theme={user.theme}>
       <div className={styles.AfWelcomRegWr}>
         <RegistrationForm />
       </div>
